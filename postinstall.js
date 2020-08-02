8c5ba1e9a267bf5817aa019b3d90e9922f640c7f
Rifqi <rifweb.android@gmail.com>
2020-08-02 07:42:22 +0700

    Renaming Xcode project

--- postinstall.js
+++ postinstall.js
@@ -11,3 +11,4 @@ lnk.sync(path.join(dir, 'src', appDirName), path.join(dir))
 
 // Showing users how to change name
 console.log("\x1b[43m", "\x1b[30m", "ADVICE:", "\x1b[0m", "See https://stackoverflow.com/questions/43293234/changing-the-package-name-in-a-react-native-app how to rename this project. Be careful, because some of the android files removed due to bug.")
+console.log("\x1b[43m", "\x1b[30m", "ADVICE:", "\x1b[0m", "See https://stackoverflow.com/questions/33370175/how-do-i-completely-rename-an-xcode-project-i-e-inclusive-of-folders how to rename XCode peoject. Or see my commit on what's changed in Rename XCode.")
--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, SafeAreaView } from 'react-native';
 
 const styles = StyleSheet.create({
     box: { padding: 10 },
@@ -8,21 +8,23 @@ const styles = StyleSheet.create({
 
 export default _ => {
     return (
-        <View style={[styles.box, {
-            height: '100%'
-        }]}>
-            <View style={{ flex: 1 }}>
-                <Text style={styles.text}>Hello world!</Text>
-                <Text>Try to run `yarn run:web`, `yarn run:android` and see the UI changes while you change app/App.js file!</Text>
-                <Text style={{
-                    marginTop: 15
-                }}>To change your app name, install `react-native-rename`</Text>
+        <SafeAreaView>
+            <View style={[styles.box, {
+                height: '100%'
+            }]}>
+                <View style={{ flex: 1 }}>
+                    <Text style={styles.text}>Hello world!</Text>
+                    <Text>Try to run `yarn run:web`, `yarn run:android` and see the UI changes while you change app/App.js file!</Text>
+                    <Text style={{
+                        marginTop: 15
+                    }}>To change your app name, install `react-native-rename`</Text>
+                </View>
+                <View>
+                    <Button title="Click Me!" onPress={_ => {
+                        alert("Button Clicked!")
+                    }}></Button>
+                </View>
             </View>
-            <View>
-                <Button title="Click Me!" onPress={_ => {
-                    alert("Button Clicked!")
-                }}></Button>
-            </View>
-        </View>
+        </SafeAreaView>
     )
 };
